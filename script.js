fd048b9b193df083786eebef10bddbcd44d29b2e
Jayanta Ojha <jayantaojha686@gmail.com>
2024-01-25 20:18:59 +0530

    Math Calculator and Currency Converted are added to CALC

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const body = document.querySelector('body');
+
+// lifestyle
+const mathCalculator = document.querySelector('#math-calculator');
 const ageCalculator = document.querySelector('#age-calculator');
 const ageComparison = document.querySelector('#age-comparison');
 const nextBirthday = document.querySelector('#next-birthday');
 const bmi = document.querySelector('#bmi');
+
+
+mathCalculator.addEventListener('click', () => {
+    window.location.href = 'https://jayantaojha.github.io/Calculator/disc/index.html';
+})
 
 ageCalculator.addEventListener('click', () => {
     window.location.href = 'age-cal.html';
@@ -20,3 +28,12 @@ bmi.addEventListener('click', () => {
     window.location.href = 'bmi.html';
 })
 
+
+// finance 
+
+const currency = document.querySelector('#currency');
+
+
+currency.addEventListener('click', () => {
+    window.location.href = 'https://jayantaojha.github.io/Currency-Converter/';
+})
